586144bf05375c9d4523d9cfcba6a42892b30021
Morgan Stuart <morgansstuart243@gmail.com>
2019-08-17 21:39:41 -0400

    remove mock dep - breaking CI?

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(name='InteractiveDataTree',
       version='.1',
       #py_modules=['interactive_data_repo'],
       packages=['interactive_data_tree'],
-      install_requires=['pandas', 'tables', 'mock', 'pygments']
+      install_requires=['pandas', 'tables',
+                        'pygments']
       )
 
